053a1e76b06d2bb99889870f02a23d6d2a539fec
Presha Sunar <preshasuna@gmail.com>
2025-02-08 12:22:09 +0545

    Added username button in homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import './App.css'
 import { Route, Routes, useNavigate,useLocation} from 'react-router-dom'
 import List from './List'
 import AddBlog from './AddBlog'
 import DetailPage from './DetailPage'
+import { UserContext } from './Context'
 // import SignUp from './App'
 
 
@@ -16,12 +17,21 @@ function Home(){
     // const location=useLocation();
     // const {signUname}=location.state || {};
     // console.log(signUname || "No username passed");
+
+    const user=useContext(UserContext);
+    const email=user.email;
+    const splitted=email.split("@");
+    const name=splitted[0];
+
+
+    // console.log("This is for context data:",{name});
    
     return(
         <>
         <div className='main'>
         <span></span>
         {/* <h3>{signUname}</h3> */}
+        <button className='uName'>{name}</button>
         <h1 className='header'>Blog App</h1>
         <button className='addBlog' onClick={()=>navigate("/home/add",{state:{uID:uID}})}>Add Blog</button>
         <button onClick={()=>navigate("/")}>Logout</button>
